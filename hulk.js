7bdc78f8ef53de9358b6deee4265f029af82dbb1
Salvatore Vella <salvatorevella1234@gmail.com>
2022-11-21 19:25:39 +0100

    Feat: first accordion complete

--- hulk.js
+++ hulk.js
@@ -1,11 +1,13 @@
 const $accordionBtn = document.querySelectorAll(".circle");
 const $verticalLine = document.querySelectorAll(".line1");
 const $horizontalLine = document.querySelectorAll(".line2");
+const $accordion = document.querySelectorAll(".accordion");
 
 
 for (let i = 0; i < $accordionBtn.length; i++) {
     $accordionBtn[i].addEventListener("click", () => {
         $verticalLine[i].classList.toggle("line1-anim");
         $horizontalLine[i].classList.toggle("line2-anim");
+        $accordion[i].classList.toggle("accordion0");
         })
 }
