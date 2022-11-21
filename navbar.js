32d5cc04b1f74719d8f2a1f3df3073aba30ee938
Chiara Arrivas <ritarrivas96@gmail.com>
2022-11-21 16:54:23 +0100

    Aggiunto evento nella naavbar

--- navbar.js
+++ navbar.js
@@ -1,7 +1,24 @@
-const $sub_menu = document.querySelector(".primo");
-$sub_menu.addEventListener("click",()=> {
-    const $prova_acquista =  document.querySelector(".sub-menu ")
-    $prova_acquista.classList.toggle("hidden");
-$sub_menu.classList.toggle("hover")
+const $acquista = document.querySelector(".primo");
+$acquista.addEventListener("click", () => {
+    const $sub_menu_acquista = document.querySelector(".sub-menu-acquista")
+    $sub_menu_acquista.classList.toggle("hidden");
+    $acquista.classList.toggle("hover")
 }
 );
+
+const $scopri = document.querySelector(".secondo");
+$scopri.addEventListener("click", () => {
+    const $sub_menu_scopri = document.querySelector(".sub-menu-scopri")
+    $sub_menu_scopri.classList.toggle("hidden");
+    $scopri.classList.toggle("hover")
+}
+);
+
+const $aiuto = document.querySelector(".terzo");
+$aiuto.addEventListener("click", () => {
+    const $sub_menu_aiuto = document.querySelector(".sub-menu-aiuto")
+    $sub_menu_aiuto.classList.toggle("hidden");
+    $aiuto.classList.toggle("hover")
+}
+);
+
