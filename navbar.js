52148a275f81eaa6f22be3e3fa00ded9aa910f71
Chiara Arrivas <ritarrivas96@gmail.com>
2022-11-22 18:15:21 +0100

    Aggiunto evento nella naavbar-02

--- navbar.js
+++ navbar.js
@@ -1,6 +1,33 @@
+const $other_menu = document.querySelectorAll(".other-menu");
+const $x_button = document.querySelectorAll(".x");
+const $search_btn = document.querySelector(".search-btn");
+const $mostra_cerca =  document.querySelector(".mostra-cerca");
+const $x_src = document.querySelector(".x-src");
+
+$search_btn.addEventListener("click", () => $mostra_cerca.classList.toggle("cerca"));
+$search_btn.addEventListener("click", () => $x_src.classList.toggle("cerca"));
+
+$x_button.forEach((el) => {
+    el.addEventListener("click" ,() => {
+        $other_menu.forEach((el) => {el.classList.add("hidden");
+        $acquista.classList.remove("hover");
+        $scopri.classList.remove("hover");
+        $aiuto.classList.remove("hover");
+        });
+    });
+});
+
+
 const $acquista = document.querySelector(".primo");
 $acquista.addEventListener("click", () => {
-    const $sub_menu_acquista = document.querySelector(".sub-menu-acquista")
+    const $sub_menu_acquista = document.querySelector(".sub-menu-acquista");
+    $other_menu.forEach((el) => {
+        if (el != $sub_menu_acquista) {
+            el.classList.add("hidden");
+            $scopri.classList.remove("hover");
+            $aiuto.classList.remove("hover");
+        }
+    });
     $sub_menu_acquista.classList.toggle("hidden");
     $acquista.classList.toggle("hover")
 }
@@ -8,7 +35,14 @@ $acquista.addEventListener("click", () => {
 
 const $scopri = document.querySelector(".secondo");
 $scopri.addEventListener("click", () => {
-    const $sub_menu_scopri = document.querySelector(".sub-menu-scopri")
+    const $sub_menu_scopri = document.querySelector(".sub-menu-scopri");
+    $other_menu.forEach((el) => {
+        if (el != $sub_menu_scopri) {
+            el.classList.add("hidden");
+            $acquista.classList.remove("hover");
+            $aiuto.classList.remove("hover");
+        }
+    });
     $sub_menu_scopri.classList.toggle("hidden");
     $scopri.classList.toggle("hover")
 }
@@ -16,9 +50,17 @@ $scopri.addEventListener("click", () => {
 
 const $aiuto = document.querySelector(".terzo");
 $aiuto.addEventListener("click", () => {
-    const $sub_menu_aiuto = document.querySelector(".sub-menu-aiuto")
+    const $sub_menu_aiuto = document.querySelector(".sub-menu-aiuto");
+    $other_menu.forEach((el) => {
+        if (el != $sub_menu_aiuto) {
+            el.classList.add("hidden");
+            $acquista.classList.remove("hover");
+            $scopri.classList.remove("hover");
+        }
+    });
     $sub_menu_aiuto.classList.toggle("hidden");
     $aiuto.classList.toggle("hover")
 }
 );
+
 
