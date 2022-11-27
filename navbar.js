43b00d616b6b3fa47aa1178766c0a2609ba296f8
Salvatore Vella <salvatorevella1234@gmail.com>
2022-11-27 20:23:20 +0100

    Responsive sub menu html

--- navbar.js
+++ navbar.js
@@ -12,6 +12,100 @@ const $sub_menu_aiuto = document.querySelector(".sub-menu-aiuto");
 const $src_x_input = document.querySelector(".src-x-input");
 const $first_nav = document.querySelector(".first-nav");
 const $x900px = document.querySelector(".x900px");
+const $tema = document.getElementById("tema");
+const $eta = document.getElementById("eta");
+const $prezzi = document.getElementById("prezzi");
+const $merchandising = document.getElementById("merchandising");
+const $interessi = document.getElementById("interessi");
+const $pickBuild = document.getElementById("pick-build");
+const $valori = document.getElementById("valori");
+const $app = document.getElementById("app");
+const $riviste = document.getElementById("riviste");
+const $setTema = document.querySelector(".set-per-tema");
+const $subEta = document.querySelector(".sub-eta");
+const $subPrezzi = document.querySelector(".sub-prezzi");
+const $subMerchandising = document.querySelector(".sub-merchandising");
+const $subInteressi = document.querySelector(".sub-interessi");
+const $subPickBuild = document.querySelector(".subPickBuild");
+const $subValori = document.querySelector(".sub-valori");
+const $subApp = document.querySelector(".sub-app");
+const $subRiviste = document.querySelector(".sub-riviste");
+const $subSubMenu = document.querySelectorAll(".sub-sub-menu");
+
+$tema.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $setTema) {
+            el.classList.add("none")
+        }
+    });
+    $setTema.classList.toggle("none")
+})
+$eta.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subEta) {
+            el.classList.add("none")
+        }
+    });
+    $subEta.classList.toggle("none")
+})
+$prezzi.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subPrezzi) {
+            el.classList.add("none")
+        }
+    });
+    $subPrezzi.classList.toggle("none")
+})
+$merchandising.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subMerchandising) {
+            el.classList.add("none")
+        }
+    });
+    $subMerchandising.classList.toggle("none")
+})
+$interessi.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subInteressi) {
+            el.classList.add("none")
+        }
+    });
+    $subInteressi.classList.toggle("none")
+})
+$pickBuild.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subPickBuild) {
+            el.classList.add("none")
+        }
+    });
+    $subPickBuild.classList.toggle("none")
+})
+$valori.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subValori) {
+            el.classList.add("none")
+        }
+    });
+    $subValori.classList.toggle("none")
+})
+$app.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subApp) {
+            el.classList.add("none")
+        }
+    });
+    $subApp.classList.toggle("none")
+})
+$riviste.addEventListener("click", () => {
+    $subSubMenu.forEach(el => {
+        if (el != $subRiviste) {
+            el.classList.add("none")
+        }
+    });
+    $subRiviste.classList.toggle("none")
+})
+
+
 
 $x900px.addEventListener("click", () => $first_nav.style.display = "none");
 
@@ -37,15 +131,16 @@ $x_src.addEventListener("click", () => {
 
 //EVENTO TASTO X NEI LINK
 $x_button.addEventListener("click", () => {
-        $other_menu.classList.add("hidden");
-        });
+    $other_menu.classList.add("hidden");
+});
 
 //EVENTI TASTI ACQUISTA SCOPRI E AIUTO
 function menu(button, menuContainer, menu) {
     button.addEventListener("click", () => {
-        if (menuContainer.classList.contains("hidden")){
+        $subSubMenu.forEach(el => el.classList.add("none"));
+        if (menuContainer.classList.contains("hidden")) {
             menuContainer.classList.toggle("hidden");
-        } 
+        }
         menu.classList.toggle("hidden");
         button.classList.toggle("hover");
     }
@@ -75,7 +170,7 @@ document.addEventListener('mouseup', function (e) {
 
     closeMenu($sub_menu_aiuto, $other_menu, $aiuto);
 
-    if(!$x_src.contains(e.target)) {
+    if (!$x_src.contains(e.target)) {
         $mostra_cerca.classList.add("cerca");
         $x_src.classList.add("cerca");
         $search_btn.style.borderBottomRightRadius = "3rem";
